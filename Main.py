48b619d8b00866d2bb8e51b82ebc04c066b4307f
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-12-03 14:50:48 -0300

    Text based interface created on main

--- Main.py
+++ Main.py
@@ -1,7 +1,19 @@
 import Functions as fc
 
-print("First List: ")
-first_list = fc.createList()
-print("Second List: ")
-seccond_list = fc.createList()
-print(fc.sumLists(first_list, seccond_list))
+while True:
+    letra = input("Choose the function: Factorial[F] || FizzBuzz[B] || sumLists[S]: ")[0].upper()
+
+    match letra:
+        case "F":
+            n = int(input("Input a number: "))
+            fc.factorial(n)
+        case "B":
+            my_dict = fc.make_dict()
+            n = int(input("Input how many iterations: "))
+            fc.fizzbuzz(n, my_dict)
+        case "S":
+            print("First List: ")
+            first_list = fc.createList()
+            print("Second List: ")
+            seccond_list = fc.createList()
+            print(fc.sumLists(first_list, seccond_list))
